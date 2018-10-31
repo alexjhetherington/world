9fb64006a44695de2331ef461e62663812f7738d
Alex Hetherington <alexjhetherington@gmail.com>
2018-10-31 23:24:26 +0000

    Add snapshot of filled world, tools for filming

---
+++ workers/world unity workers/Assets/Gamelogic/Camera/ZoomOut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour {
+
+    [SerializeField] private float speed = 2;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update () {
+        if (Input.GetKey(KeyCode.F2))
+        {
+            cam.orthographicSize += speed * Time.deltaTime;
+        }
+	}
+}
